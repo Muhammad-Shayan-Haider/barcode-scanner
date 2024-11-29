15b384e37f2e99828a097374e14484c962a4ddcf
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-11-29 14:03:49 +0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Muhammad Shayan on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
