d52a0a8670dad430dadec17cef964e0e32801405
Muhammad Shayan <shayanhaider1999@gmail.com>
2024-11-29 14:36:25 +0500

    initial UI setup

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Muhammad Shayan on 29/11/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not yet scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
