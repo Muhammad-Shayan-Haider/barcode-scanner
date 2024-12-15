6ef802ee21a5813f341eb81b2f326f5026b05d19
Muhammad Shayan <shayanhaider1999@gmail.com>
2024-12-15 15:10:56 +0500

    remove maxWidth, and use Text instead of string

--- BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -16,7 +16,7 @@ struct BarcodeScannerView: View {
             VStack {
                 ScannerView(scannedBarcode: $viewModel.scannedBarcode,
                             alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .frame(maxHeight: 300)
                 
                 Spacer()
                     .frame(height: 60)
@@ -34,8 +34,8 @@ struct BarcodeScannerView: View {
             }
             .navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title),
-                      message: Text(alertItem.message),
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
                       dismissButton: alertItem.dismissButton)
             }
         }
